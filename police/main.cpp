1e8b0315a52b04f2479f62a6f45c55c569e21595
alexxmir81 <_alexx@mail.ru>
2022-04-08 16:08:24 +0300

    save

--- police/main.cpp
+++ police/main.cpp
@@ -3,9 +3,10 @@
 #include<map>
 #include<fstream>
 #include<list>
+#include<windows.h>
 
 using namespace std;
-
+#define tab "\t"
 const std::map<int, std::string> CRIMES =
 {
 	std::pair<int,std::string>(1, "проезд на красный свет"),
@@ -22,14 +23,113 @@ class Crime
 {
 	int id; //статья
 	std::string place; //место происшествоия
+public:
+	int get_id()const
+	{
+		return id;
+	}
+	const std::string& get_place()const
+	{
+		return place;
+	}
 	Crime(int id, std::string place) : id(id), place(place) {}
 	~Crime() {}
-
-
+	
 };
+
+std::ostream& operator<<(std::ostream& os, const Crime& obj)
+{
+	return os << CRIMES.at(obj.get_id()) << " - " << obj.get_place();
+};
+std::ofstream& operator<<(std::ofstream& ofs, const Crime& obj)
+{
+	ofs << obj.get_id() << obj.get_place();
+	return ofs;
+}
+
+void print(const map<std::string, std::list<Crime>>& base);
+int check_crime();
+std::string input_place();
+void save(const map<std::string, std::list<Crime>>& base, std::string filename);
 
 void main()
 {
-	setlocale(LC_ALL, "");
+	//setlocale(LC_ALL, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::map<std::string, std::list<Crime>> base =
+	{
+		std::pair<std::string, std::list<Crime>>("a777bb", {Crime(1, "ул.Ленина"), Crime(2,"ул.Октябрьская"), Crime(3,"ул.Октябрьская"),Crime(6,"ул.Степного Фронта")}),
+		{"b123ca",{Crime(5,"ул.Ленина"), Crime(8,"ул.Октябрьская")}},
+		{"b123ab",{Crime(4,"перекресток Ленина и Октябрьской"), Crime(8,"ул.Октябрьская")}}
 
+	};
+	print(base);
+	save(base, "base.txt");
+#ifdef INPUT_BASE
+	std::string licence_plate; //номерной знак
+	cout << "Введите номер автомобиля: ";cin >> licence_plate;
+	//int id;
+	//cout << "Введите правонарушение: ";cin >> id;
+	//std::string place;
+	//cout << "Введите место правнарушения: ";
+	//cin.ignore();
+	//std::getline(cin, place);
+	base[licence_plate].push_back(Crime(check_crime(), input_place()));
+	print(base);
+#endif // INPUT_BASE
+
+
+}
+
+void print(const map<std::string, std::list<Crime>>& base)
+{
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end();++it)
+	{
+		cout << it->first << "\n";
+		for (std::list<Crime>::const_iterator jt = it->second.begin(); jt != it->second.end(); ++jt)
+		{
+			cout << *jt << "\n";
+		}
+		cout << "\n--------------------------------------\n";
+	}
+}
+
+int check_crime()
+{
+	system("CLS");
+	for (std::pair<int, std::string>i : CRIMES)
+	{
+		cout << i.first << tab << i.second << endl;
+	}
+	int id;
+	cout << "Выберите правонарушение: "; cin >> id;
+	return id;
+}
+std::string input_place()
+{
+	std::string place;
+	cout << "Введите место правнарушения: ";
+	cin.ignore();
+	std::getline(cin, place);
+	return place;
+}
+
+void save(const map<std::string, std::list<Crime>>& base, std::string filename)
+{
+	std::ofstream fout(filename);
+	for (std::map<std::string, std::list<Crime>>::const_iterator it = base.begin(); it != base.end(); ++it)
+	{
+		fout << it->first << ":";
+		for (std::list<Crime>::const_iterator jt = it->second.begin();jt != it->second.end(); ++jt)
+		{
+			fout << *jt << ",";
+		}
+		fout.seekp(-1, std::ios::cur); //cur - current (текущий)
+		fout << ";\n";
+	}
+	fout.close();
+	std::string command = "notepad ";
+	command += filename;
+	system(command.c_str()); //c_str - возвращает содержимое объекта std::string в виде массива элементов char(c-string)
 }
